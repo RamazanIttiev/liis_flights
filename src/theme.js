73df2393dac08e13aa97b2bf700aadebe4973e38
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-08-07 13:17:47 +0300

    theme optimizations

--- src/theme.js
+++ src/theme.js
@@ -1,13 +1,7 @@
 const theme = {
-  typography: {
-    fontSize: 14,
-
-    h1: {
-      fontFamily: 'Roboto',
-      fontStyle: 'normal',
-      fontSize: 24,
-      fontWeight: 500,
-      lineHeight: 28,
+  palette: {
+    primary: {
+      main: '#424242',
     },
   },
 };
