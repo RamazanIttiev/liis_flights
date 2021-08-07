fe858f72a29ceffe13d032d63928d2b942bdbfba
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-08-07 10:29:47 +0300

    added themeProvider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import App from './App/App';
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
+import theme from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
---
+++ src/theme.js
@@ -0,0 +1,15 @@
+const theme = {
+  typography: {
+    fontSize: 14,
+
+    h1: {
+      fontFamily: 'Roboto',
+      fontStyle: 'normal',
+      fontSize: 24,
+      fontWeight: 500,
+      lineHeight: 28,
+    },
+  },
+};
+
+export default theme;
