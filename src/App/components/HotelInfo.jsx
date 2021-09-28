1a497882790406436434c557925ee0abc28040db
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-09-28 11:20:29 +0300

    house icon size changes

--- src/App/components/HotelInfo.jsx
+++ src/App/components/HotelInfo.jsx
@@ -38,6 +38,11 @@ const PlaneImg = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+
+  img {
+    width: 35px;
+    height: 35px;
+  }
 `;
 
 const Info = styled.div`
