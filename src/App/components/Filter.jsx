d0136edf1328102ed4472123324d154c4121ea82
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-09-28 12:16:59 +0300

    filter form mark up

---
+++ src/App/components/Filter.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 360px;
+  min-height: 410px;
+  background: #ffffff;
+  box-shadow: 0px 4px 33px rgb(0 0 0 / 4%);
+  border-radius: 16px;
+  padding: 32px;
+`;
+
+const Label = styled.label`
+  margin-top: 25px;
+  margin-bottom: 7px;
+  position: relative;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 16px;
+  color: ${props => props.error && '#EB1717'};
+`;
+
+const Input = styled.input`
+  border: 1px solid #c9cacc;
+  box-sizing: border-box;
+  border-radius: 4px;
+  height: 34px;
+  border: ${props => props.error && '1px solid #EB1717'};
+  box-sizing: border-box;
+  box-shadow: 0px 0px 4px rgba(235, 23, 23, 0.2);
+  border-radius: 4px;
+`;
+
+const StyledButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  width: 116px;
+  height: 34px;
+  border: none;
+  margin-left: auto;
+  margin-top: 25px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  color: #fff;
+  background: linear-gradient(104.34deg, #41522e -15.34%, #be8022 145.95%);
+  box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.15);
+  border-radius: 4px;
+
+  &:hover {
+    box-shadow: 0px 0px 4px #99a0a3;
+  }
+
+  &:active {
+    border: 1.5px solid rgba(29, 138, 216, 0.3);
+  }
+`;
+
+const Filter = () => (
+  <StyledForm>
+    <Label>Локация</Label>
+    <Input type="text" />
+    <Label>Дата заселения</Label>
+    <Input type="date" />
+    <Label>Количество дней</Label>
+    <Input type="number" />
+    <StyledButton type="submit">Найти</StyledButton>
+  </StyledForm>
+);
+
+export default Filter;
