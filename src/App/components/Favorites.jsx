1782128e0bca825f82ca65ce34f24e101899010d
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-09-29 12:02:18 +0300

    Sort buttons mark up

--- src/App/components/Favorites.jsx
+++ src/App/components/Favorites.jsx
@@ -4,6 +4,9 @@ import { connect } from 'react-redux';
 import styled from 'styled-components';
 import HotelInfo from './HotelInfo';
 
+import ArrowUp from '../../assets/arrowUp.svg';
+import ArrowDown from '../../assets/arrowDown.svg';
+
 const StyledForm = styled.form`
   display: flex;
   flex-direction: column;
@@ -16,16 +19,60 @@ const StyledForm = styled.form`
 `;
 
 const Title = styled.div``;
+
+const SortWrapper = styled.div`
+  display: flex;
+  margin-top: 32px;
+`;
+
+const SortBtn = styled.div`
+  cursor: pointer;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 4px 8px;
+  background: #ffffff;
+  border: 1px solid #41522e;
+  box-sizing: border-box;
+  border-radius: 4px;
+
+  &:first-child {
+    margin-right: 8px;
+  }
+`;
+
+const Arrows = styled.div`
+  margin-left: 8px;
+  display: flex;
+  flex-direction: column;
+`;
+
 const Favorites = ({ favourites }) => (
   <StyledForm>
     <Title>Избранное</Title>
-    {favourites.map(hotel => (
+    <SortWrapper>
+      <SortBtn>
+        Рейтинг
+        <Arrows>
+          <img src={ArrowUp} alt="" />
+          <img src={ArrowDown} alt="" />
+        </Arrows>
+      </SortBtn>
+      <SortBtn>
+        Цена
+        <Arrows>
+          <img src={ArrowUp} alt="" />
+          <img src={ArrowDown} alt="" />
+        </Arrows>
+      </SortBtn>
+    </SortWrapper>
+    {/* {favourites.map(hotelId => (
       <HotelInfo
-        key={hotel.hotelId}
-        isFavourite={favourites.length > 0 && favourites.indexOf(hotel.hotelId) === -1}
-        hotel={hotel}
+        key={hotelId}
+        isFavourite={favourites.length > 0 && favourites.indexOf(hotelId) === -1}
+        hotelId={hotelId}
       />
-    ))}
+    ))} */}
   </StyledForm>
 );
 
